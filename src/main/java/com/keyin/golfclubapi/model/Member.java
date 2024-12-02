6d28908d21066a7da18084d6eeac0f2832046f90
Pavel Pliuiko <pavel.pliuiko@keyin.com>
2024-12-02 17:34:27 -0330

    added models

---
+++ src/main/java/com/keyin/golfclubapi/model/Member.java
@@ -0,0 +1,78 @@
+package com.keyin.golfclubapi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String address;
+    private String email;
+    private String phoneNumber;
+    private LocalDate membershipStartDate;
+    private Integer membershipDuration;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDate getMembershipStartDate() {
+        return membershipStartDate;
+    }
+
+    public void setMembershipStartDate(LocalDate membershipStartDate) {
+        this.membershipStartDate = membershipStartDate;
+    }
+
+    public Integer getMembershipDuration() {
+        return membershipDuration;
+    }
+
+    public void setMembershipDuration(Integer membershipDuration) {
+        this.membershipDuration = membershipDuration;
+    }
+}
